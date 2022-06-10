3a58d6698218c87804d2d0dbe1be2336ea061a67
AleksandrSmirnov92 <ryan00@mail.ru>
2022-06-10 14:07:03 +0300

    react доделываю приложение

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -5,11 +5,12 @@ import {Header} from "./components/header/header"
 import {AddTaskMenu} from "./components/addTaskMenu/addTaskMenu"
 import {Filter} from "./components/filter/filter"
 import {AllTask} from "./components/allTask/allTask"
+import { render } from '@testing-library/react';
 function App() {
  let storage = []
  let count = 0
  let allFilter = "ALL"
- let checked = false
+ let checked = false 
   if (localStorage.getItem("todo")) {
     storage = JSON.parse(localStorage.getItem("todo"));
   }
@@ -20,11 +21,12 @@ function App() {
   let [counter,setCounter] = useState(count)
   let [filter,setFilter] = useState(allFilter)
 
+
   localStorage.setItem("todo", JSON.stringify(addMessage))
   localStorage.setItem("counter", JSON.stringify(counter))
 
   let addTask = (input) => {
-    setaddMessage([...addMessage,{id: Math.random(), massage: input, checked: checked }])
+    setaddMessage([...addMessage,{id: Math.random(), massage: input, checked: checked ,changeColor:false}])
     setCounter(counter + 1)
     localStorage.setItem("todo", JSON.stringify(addMessage))
     localStorage.setItem("counter", JSON.stringify(counter))
@@ -35,26 +37,50 @@ function App() {
         addMessage.id !== id,
         localStorage.setItem("todo", JSON.stringify(addMessage))
     )])
+
+    for (let item of addMessage) {
+      if (item.id === id && item.checked === false) {
+        setCounter(counter - 1)
+        localStorage.setItem("counter", JSON.stringify(counter))
+    break
+    }
+  }
+    // addMessage.forEach((item)=>{
+    //   if (item.id === id && item.checked === false) {
+    //     setCounter(counter - 1)
+    // localStorage.setItem("counter", JSON.stringify(counter))
+    //   }
+    // })
     
-    setCounter(counter - 1)
-    localStorage.setItem("counter", JSON.stringify(counter))
+    
  }
  let changeFilter = (value) => {
       setFilter(filter = value)
       localStorage.setItem("Filter", JSON.stringify(filter))
  }
- let changeCheked = () => {
+ let changeCheked = (id) => {
   addMessage.forEach(element => {
-    element.checked = !element.checked
+    if (element.id === id) {
+      setTimeout(()=>{
+        element.checked = !element.checked
+        setaddMessage([...addMessage])
+        localStorage.setItem("todo", JSON.stringify(addMessage))
+        
+      },900)
+      element.changeColor = !element.changeColor
+      if (element.changeColor) {
+        setCounter(counter - 1)
+      }
+      else {
+        setCounter(counter +1)
+      }
+      // setaddMessage([...addMessage])
+    }
   });
+  // setaddMessage([...addMessage])
    localStorage.setItem("todo", JSON.stringify(addMessage))
  }
-  // setaddMessage([...addMessage, addMessage.forEach(element => {
-  //   element.checked = !element.checked
-  // })
- 
-
-
+  
 
   return (
    
--- react-hooks/src/components/allTask/allTask.js
+++ react-hooks/src/components/allTask/allTask.js
@@ -1,66 +1,96 @@
-import React from "react"
-import AllTaskCSS from "./allTask.module.css"
-export const AllTask = ({addMessage,removeTask,filter,changeCheked}) => {
-
-   
+import React from "react";
+import AllTaskCSS from "./allTask.module.css";
+export const AllTask = ({ addMessage, removeTask, filter, changeCheked ,changeColorItem}) => {
   let remove = (id) => {
-    removeTask(id) 
-     }
-  let checkbox = () => {
+    removeTask(id);
+  };
+  let checkbox = (id) => {
     // alert("стоит")
-    changeCheked()
-  }
-  
-   
-   return (
-      addMessage.map((x,index)=>{
-        if (filter === "ALL") {
-         return (
-            <div key={x.id} className={AllTaskCSS.add_task_result1}  >
-            <span id = {x.id} className={AllTaskCSS.add_task_result_text}>
-               {x.massage}
-            </span>
-            <div>
-              <input id = {x.id} type="checkbox" onClick={checkbox}/>
-              <button id={x.id} className={AllTaskCSS.add_task_result_text_remove} onClick = {() => {remove(x.id)}}>
-                удалить 
-              </button>
-            </div>
+    changeCheked(id);
+  };
+
+  return addMessage.map((x, index) => {
+    if (filter === "ALL") {
+      
+      return (
+        <div
+          key={x.id}
+          className={`${AllTaskCSS.add_task_result1} 
+            ${
+              x.changeColor
+                ? AllTaskCSS.add_task_chacked_color
+                : AllTaskCSS.add_task_result1
+            }`}
+        >
+          <span id={x.id} className={`${x.changeColor ? AllTaskCSS.add_task_result_text_t : AllTaskCSS.add_task_result_text}`}>
+            {x.massage}
+          </span>
+          <div>
+            <input id={x.id} type="checkbox" onClick={() => checkbox(x.id)} defaultChecked={x.checked ? true : false}/>
+            <button
+              id={x.id}
+              className={AllTaskCSS.add_task_result_text_remove}
+              onClick={() => {
+                remove(x.id);
+              }}
+            >
+              удалить
+            </button>
           </div>
-         )
-        }
-        if (filter === "ALLACTIVE" && x.checked === false) {
-          return (
-            <div key={x.id} className={AllTaskCSS.add_task_result1}  >
-            <span id = {x.id} className={AllTaskCSS.add_task_result_text}>
-               {x.massage}
-            </span>
-            <div>
-              <input id = {x.id} type="checkbox"  onClick={checkbox}/>
-              <button id={x.id} className={AllTaskCSS.add_task_result_text_remove} onClick = {() => {remove(x.id)}}>
-                удалить 
-              </button>
-            </div>
+        </div>
+      );
+    }
+    if (filter === "ALLACTIVE" && x.checked === false) {
+      return (
+        <div key={x.id}  className={`${AllTaskCSS.add_task_result1} 
+        ${
+          x.changeColor
+            ? AllTaskCSS.add_task_chacked_color
+            : AllTaskCSS.add_task_result1
+        }`}>
+          <span id={x.id} className={`${x.changeColor ? AllTaskCSS.add_task_result_text_t : AllTaskCSS.add_task_result_text}`}>
+            {x.massage}
+          </span>
+          <div>
+            <input id={x.id} type="checkbox" onClick={() => checkbox(x.id)} defaultChecked={x.checked ? true : false}/>
+            <button
+              id={x.id}
+              className={AllTaskCSS.add_task_result_text_remove}
+              onClick={() => {
+                remove(x.id);
+              }}
+            >
+              удалить
+            </button>
           </div>
-         )
-        }
-        if (filter === "ALLINACTIVE" && x.checked === true) {
-          return (
-            <div key={x.id} className={AllTaskCSS.add_task_result1}  >
-            <span id = {x.id} className={AllTaskCSS.add_task_result_text}>
-               {x.massage}
-            </span>
-            <div>
-              <input id = {x.id} type="checkbox"  onClick={checkbox}/>
-              <button id={x.id} className={AllTaskCSS.add_task_result_text_remove} onClick = {() => {remove(x.id)}}>
-                удалить 
-              </button>
-            </div>
+        </div>
+      );
+    }
+    if (filter === "ALLINACTIVE" && x.checked === true) {
+      return (
+        <div key={x.id} className={`${AllTaskCSS.add_task_result1} 
+        ${
+          x.changeColor
+            ? AllTaskCSS.add_task_chacked_color
+            : AllTaskCSS.add_task_result1
+        }`}>
+          <span id={x.id} className={`${x.changeColor ? AllTaskCSS.add_task_result_text_t : AllTaskCSS.add_task_result_text}`}>
+            {x.massage}
+          </span>
+          <div>
+            <input id={x.id} type="checkbox" onClick={() => checkbox(x.id)} defaultChecked={x.checked ? true : false}/>
+            <button
+              id={x.id}
+              className={AllTaskCSS.add_task_result_text_remove}
+              onClick={() => {
+                remove(x.id);
+              }}
+            >
+              удалить
+            </button>
           </div>
-         )
-        }
-      })
-        );
-   
-   
-}
+        </div>
+      );
+    }
+  });
+};
