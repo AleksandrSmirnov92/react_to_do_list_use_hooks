ebf614233a72a1ddd57b74a0976ce8225a0bded0
AleksandrSmirnov92 <ryan00@mail.ru>
2022-07-12 21:58:41 +0300

    исправил eslint

--- react-hooks/src/components/filter/filter.js
+++ react-hooks/src/components/filter/filter.js
@@ -1,19 +1,29 @@
-import React, { createRef } from "react";
-import FilterCSS from "./filter.module.css"
-export const Filter = ({changeFilter}) => {
-   let valuee = React.createRef()
-   let change = () => {
-      changeFilter(valuee.current.value)
-      // alert(`значение +${ valuee.current.value}`)
-   }
-   return(
-      <div className={FilterCSS.filter_block}>
-      <select ref={valuee} onChange={change}  className={FilterCSS.filter} defaultValue={'DEFAULT'} >
-         <option id="1" value="ALL" selected>Выбрать все </option>
-         <option id="2"  value={"ALLACTIVE"} > Выбрать активные </option>
-         <option id="3"  value={"ALLINACTIVE"} >Выбрать законченные</option>
+import React, { createRef } from 'react';
+import FilterCSS from './filter.module.css';
+export const Filter = ({ changeFilter }) => {
+  const valuee = React.createRef();
+  const change = () => {
+    changeFilter(valuee.current.value);
+    // alert(`значение +${ valuee.current.value}`)
+  };
+  return (
+    <div className={FilterCSS.filter_block}>
+      <select
+        ref={valuee}
+        onChange={change}
+        className={FilterCSS.filter}
+        defaultValue={'DEFAULT'}>
+        <option id="1" value="ALL" selected>
+          Выбрать все{' '}
+        </option>
+        <option id="2" value={'ALLACTIVE'}>
+          {' '}
+          Выбрать активные{' '}
+        </option>
+        <option id="3" value={'ALLINACTIVE'}>
+          Выбрать законченные
+        </option>
       </select>
-      
-   </div>
-   )
-}
+    </div>
+  );
+};
