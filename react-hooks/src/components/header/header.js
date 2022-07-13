649bb0004e979c944d352e29050ede99c8166b2a
AleksandrSmirnov92 <ryan00@mail.ru>
2022-07-13 16:11:39 +0300

    n

--- react-hooks/src/components/header/header.js
+++ react-hooks/src/components/header/header.js
@@ -1,15 +1,14 @@
-import React from "react"
-import HeaderCSS from "./header.module.css"
+import React from 'react';
+import HeaderCSS from './header.module.css';
 
-export const Header = ({counter}) => {
-   return(
-      <header className={HeaderCSS.header}>
-        
+export const Header = ({ counter }) => {
+  return (
+    <header className={HeaderCSS.header}>
       <div className={HeaderCSS.header_wrapper}>
-         <span className={HeaderCSS.header_span}>Осталось выполнить</span>
-   
-         <span className={HeaderCSS.header_span2}>{counter}</span>
+        <span className={HeaderCSS.header_span}>Осталось выполнить</span>
+
+        <span className={HeaderCSS.header_span2}>{counter}</span>
       </div>
-   </header>
-   )
-}
+    </header>
+  );
+};
